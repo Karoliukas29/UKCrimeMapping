2047879d6afcbbc065c60e042310eb509bb8aeb7
karoliukas29 <karolism18@gmail.com>
2024-09-12 18:40:20 +0100

    added CrimeMapper.kt

---
+++ app/src/main/java/com/karolisstuff/ukcrimemapping/data/mapper/CrimeMapper.kt
@@ -0,0 +1,13 @@
+package com.karolisstuff.ukcrimemapping.data.mapper
+
+import com.karolisstuff.ukcrimemapping.data.model.CrimeResponseItem
+import com.karolisstuff.ukcrimemapping.domain.model.Crime
+
+fun CrimeResponseItem.toDomain(): Crime {
+    return Crime(
+        category = this.category,
+        latitude = this.location.latitude.toDouble(),
+        longitude = this.location.longitude.toDouble(),
+        outcome = this.outcomeStatus?.category ?: "Unknown"
+    )
+}
